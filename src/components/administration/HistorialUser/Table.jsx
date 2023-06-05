7b5de4447485bfbaafa4b7df66579d5121fd14f0
laura1809 <ltrr2001@gmail.com>
2023-06-05 09:51:30 -0500

    add hour historial

--- src/components/administration/HistorialUser/Table.jsx
+++ src/components/administration/HistorialUser/Table.jsx
@@ -1,6 +1,6 @@
 import MUIDataTable from "mui-datatables";
 export const TableBasic = ({ data }) => {
-  const columns = ["Fecha", "Usuario", "Transacción", "Ciudad", "Sucursal"];
+  const columns = ["Fecha", "Hora","Usuario", "Transacción", "Ciudad", "Sucursal"];
 
   return (
     <MUIDataTable title={"Auditoría PAR-KUD"} data={data} columns={columns} />
